dd2706164b2f3029f888510fdb58dd38aaa0ca98
miguel lopes <mzramnadev@gmail.com>
2019-12-08 22:34:21 -0300

    funciona até o momento,mas apenas um paralelo por vez e não é possivel de se pegar o retorno de cada processo,ambos problemas a serem resolvidos posteriormente

--- file converter.py
+++ file converter.py
@@ -1,40 +1,54 @@
 import ffmpeg
 import os
-simultaneosconvert=2
+simultaneosconvert=1
 fileExtensions=["webm","flv","vob","ogg","ogv","drc","gifv","mng","avi","mov","qt","wmv","yuv","rm","rmvb","asf","amv","mp4","m4v","mp\*","m\?v","svi","3gp","flv","f4v"]
 thisdir = os.getcwd()
 convertTo={"formato":"mkv","filterParameters":
 	{"scale":"hd480",},
 		   "outputParameters":{"vcodec":"libx264","crf":"22","preset":"slow"}}
 arquivosAConverter=[]
+flagConversion="convertido"
 for r, d, f in os.walk(thisdir):
 	for file in f:
 		nome=file.split(".")
 		for extension in fileExtensions :
-			if (extension) in nome[len(nome)-1]:
-				input=os.path.join(r, file)
-				output=""
-				for i in range(0,(len(nome)-1)):
-					output+=nome[i]+"."
-				output+="convertido."+convertTo["formato"]
-				output=os.path.join(r, output)
-				arquivosAConverter.append({"input":input,"output":output})
+			if flagConversion not in nome[len(nome)-2]:
+				if extension in nome[len(nome)-1]:
+					input=os.path.join(r, file)
+					output=""
+					for i in range(0,(len(nome)-1)):
+						output+=nome[i]+"."
+					output+=flagConversion+"."+convertTo["formato"]
+					output=os.path.join(r, output)
+					arquivosAConverter.append({"input":input,"output":output})
 #for arquivos in arquivosAConverter :
 #	print("entrada= "+arquivos["input"])
 #	print("saida= "+arquivos["output"])
 processes=[]
 for arquivos in arquivosAConverter :
-	if len(processes)<=simultaneosconvert:
-		processes.append(ffmpeg.input(arquivos["input"],vcodec="h264_cuvid").\
-		filter('scale',size=convertTo["filterParameters"]["scale"]). \
-		output(arquivos["output"]#,vcodec=convertTo["outputParameters"]["vcodec"]
-			   ,crf=convertTo["outputParameters"]["crf"]
-			   ,preset=convertTo["outputParameters"]["preset"]
-			    ,hwaccel="cuvid",vcodec=["h264_nvenc"] #nvidia accelerated
-			   ,).run()#_async(cmd="ffmpeg",quiet=True)
-		)
-		print(len(processes))
+
+	processes.append(ffmpeg.input(arquivos["input"],vcodec="h264_cuvid").\
+	filter('scale',size=convertTo["filterParameters"]["scale"]). \
+	output(arquivos["output"]
+		   ,crf=convertTo["outputParameters"]["crf"]
+		   ,preset=convertTo["outputParameters"]["preset"]
+			,vcodec="h264_nvenc"
+		   ,)
+	)
+convertidos=0
+running=[]
+while convertidos< len(processes):
+	print(len(running))
+	if len(running) < simultaneosconvert:
+		print(arquivosAConverter[convertidos]["output"])
+		running.append(processes[convertidos].run_async(pipe_stdout=True, pipe_stderr=True,quiet=False,overwrite_output=True))
+		convertidos+=1
 	else:
-		for process in processes :
-			process.wait()
+		print(len(running))
+		for i in running:
+			print(i.communicate())
+			if i.poll() == None:
+				print(i.poll())
+				running.remove(i)
+
 
