ee6736e141bb48c662261b691f80f90e98c52295
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2020-06-24 15:43:45 -0300

    corrigido problema de falta de audio

--- file converter.py
+++ file converter.py
@@ -6,7 +6,7 @@ fileExtensions=["webm","flv","vob","ogg","ogv","drc","gifv","mng","avi","mov","q
 thisdir=os.getcwd()
 convertTo={"formato":"mkv","filterParameters":
 	{"scale":"hd480","fps":24},
-		   "outputParameters":{"vcodec":"h264_omx","crf":"22","preset":"slow","acodec":"copy"}}
+		   "outputParameters":{"vcodec":"h264_omx","crf":51,"preset":"slow","acodec":"copy"}}
 arquivosAConverter=[]
 for r, d, f in os.walk(thisdir):
 	for file in f:
@@ -26,15 +26,16 @@ for r, d, f in os.walk(thisdir):
 processes=[]
 for arquivos in arquivosAConverter :
 	#if len(processes)<=simultaneosconvert:
-	processes.append( ffmpeg.input(arquivos["input"]).
-	filter('scale',size=convertTo["filterParameters"]["scale"]).
-	filter('fps', fps=convertTo["filterParameters"]["fps"], round='up').
-	output(arquivos["output"],vcodec=convertTo["outputParameters"]["vcodec"]
+	audio=ffmpeg.input(arquivos["input"]).audio
+	video=ffmpeg.input(arquivos["input"]).filter('scale',size=convertTo["filterParameters"]["scale"]).filter('fps', fps=convertTo["filterParameters"]["fps"], round='up')
+	output=ffmpeg.output(video,audio,arquivos["output"]
+		   ,acodec=convertTo["outputParameters"]["acodec"]
+		   ,vcodec=convertTo["outputParameters"]["vcodec"]
 		   ,crf=convertTo["outputParameters"]["crf"]
 		   ,preset=convertTo["outputParameters"]["preset"]
-		   ,acodec=convertTo["outputParameters"]["acodec"])
-	)
+		   )
+	
+	processes.append(output)
 	#else:
 for process in processes :
-	process.run(overwrite_output=True,)
-
+	process.run(overwrite_output=True,capture_stdout=True)
