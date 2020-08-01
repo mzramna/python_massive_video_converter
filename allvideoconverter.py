6d9a7e9e26f3ada3f353a34b7b279023e204ec5f
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2020-08-01 17:37:41 -0300

    criado metodo de criação de comando de terminal para uso
    criada função que pega pelos parametros e cria uma linha de comando para que possa ser usada pelo próprio programa ou pelo usuário externamente

--- allvideoconverter.py
+++ allvideoconverter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os,subprocess,re
+import os,subprocess,re,shutil
 
 def list_content(path):
 	retorno={}
@@ -32,85 +32,124 @@ def compare_resolution(arquivo,width:int,height:int):
 	else:
 		return False
 
-def convert_video(arquivo,extension_convert:str="mkv",resize=False,resolution:int=480,remove=False,codec:str="h264_omx",fps:int=24,crf:int=20,preset:str="slow",resize_log='./resize.log',working_log="./working.log"):
-    fileExtensions=["webm","flv","vob","ogg","ogv","drc","gifv","mng","avi","mov","qt","wmv","yuv","rm","rmvb","asf","amv","mp4","m4v","mp\*","m\?v","svi","3gp","flv","f4v"]
+
+def create_command(arquivo,extension_convert:str="mkv",resize=False,resolution:int=480,codec:str="h264_omx",fps:int=24,crf:int=20,preset:str="slow",array=False,resize_log="./resize.log"):
+    fileExtensions=["webm","flv","vob","ogg","ogv","drc","gifv","mng","avi","mov","qt","wmv","yuv","rm","rmvb","asf","amv","mp4","m4v","mp\*","m\?v","svi","3gp","flv","f4v","mkv"]
     
-	file_name_no_extension=os.path.splitext(arquivo.name)[0]
-	extension=os.path.splitext(arquivo.name)[1]
-	
-	relative_dir="/".join(str(x) for x in arquivo.path.split("/")[:-1])+"/"
-	new_file_name=str(relative_dir)+str(file_name_no_extension)+"."+extension_convert
-	if ( extension =="."+extension_convert and not resize ) or ( extension not in fileExtensions ):
-		return 0
+    file_name_no_extension=os.path.splitext(arquivo.name)[0]
+    extension=os.path.splitext(arquivo.name)[1][1:]
+    print(extension)
+    relative_dir="/".join(str(x) for x in arquivo.path.split("/")[:-1])+"/"
+    new_file_name=str(relative_dir)+str(file_name_no_extension)
+
+    if ( extension == extension_convert and not resize ) or ( ( extension or extension_convert ) not in fileExtensions ):
+        print("already resized")
+        return ""
+    elif extension == extension_convert:
+        new_file_name=new_file_name+".convert"
+        same_extension=True
     else:
-        log_file=open(working_log,"a")
-		log_file.write(new_file_name+"\n")
-		log_file.close()
-	
-	command=["ffmpeg","-i",str(arquivo.path), "-acodec", "copy" ]
-	copy_command=["-vcodec","copy"]
-	resize_command=["-filter:v","fps=fps="+str(fps),"-c:v",codec,"-crf",str(crf),"-preset",preset,"-s"]
-	if not resize :
-		#print(" not resize")
-		for i in copy_command:
-			command.append(i)
-	elif resize :
-		#print("resize")
-		try:
-			log_file = open(resize_log)
-			converted=False
-			#print(log_file.readlines())
-			for line in log_file.readlines():
-				print(line)
-				if line == new_file_name+"\n":
-					converted=True
-			log_file.close()
-		except:
-			converted=False
-		if not converted:
-			#print("not resized")
-			if resolution == 480:
-				if compare_resolution(arquivo,640,480):
-					#print("resize to 480p")
-					resize_command.append("hd480")	#determina a escala do arquivo para 480p
-				else:
-					#print("menor que o pedido")
-					return 0
-			elif resolution == 720:
-				if compare_resolution(arquivo,1280,720):
-					#print("resize to 720p")
-					resize_command.append("hd720") #determina a escala do arquivo para 720p
-				else:
-					#print("menor que o pedido")
-					return 0
-			else:
-				return 0
-			print("criado comando")
-			for i in resize_command:
-				command.append(i)
-		else:
-			print("already resized")
-			return 0
-	command.append(new_file_name)
-	#print(command)
-	result = subprocess.run(command, stdout=subprocess.PIPE)
-	if resize:
+        same_extension=False
+    new_file_name=new_file_name+"."+extension_convert
+    command=["ffmpeg","-y","-i",str(arquivo.path), "-acodec", "copy" ]
+    copy_command=["-vcodec","copy"]
+    resize_command=["-filter:v","fps=fps="+str(fps),"-c:v",codec,"-crf",str(crf),"-preset",preset]
+    if not resize :
+        for i in copy_command:
+            command.append(i)
+    else:
+        try:
+            log_file = open(resize_log)
+            converted=False
+            for line in log_file.readlines():
+                print(line)
+                if line == new_file_name+"\n":
+                    converted=True
+            log_file.close()
+        except:
+            converted=False
+        if not converted:
+            if resolution == 480:
+                if compare_resolution(arquivo,640,480):
+                    resize_command.append("-s")
+                    resize_command.append("hd480") #determina a escala do arquivo para 480p
+                else:
+                    pass
+            elif resolution == 720:
+                if compare_resolution(arquivo,1280,720):
+                    resize_command.append("-s")
+                    resize_command.append("hd720") #determina a escala do arquivo para 720p
+                else:
+                    pass
+            else:
+                pass
+            for i in resize_command:
+                command.append(i)
+        else:
+            print("already resized")
+            return ""
+    command.append(new_file_name)
+    #print(command)
+    
+    if array:
+        return command
+    else:
+        retorno=""
+        for comand in command:
+            retorno+=comand+" "
+        return retorno
+
+def convert_video(arquivo,extension_convert:str="mkv",resize=False,resolution:int=480,remove=False,codec:str="h264_omx",fps:int=24,crf:int=20,preset:str="slow",resize_log='./resize.log',working_log="./working.log"):
+    file_name_no_extension=os.path.splitext(arquivo.name)[0]
+    extension=os.path.splitext(arquivo.name)[1][1:]
+    print(extension)
+    relative_dir="/".join(str(x) for x in arquivo.path.split("/")[:-1])+"/"
+    new_file_name=str(relative_dir)+str(file_name_no_extension)
+
+    if extension == extension_convert:
+        new_file_name=new_file_name+".convert"
+        same_extension=True
+    else:
+        same_extension=False
+        try:
+            log_file=open(working_log,"r")
+            for line in log_file.readlines():
+                if line == new_file_name+"\n":
+                    log_file.close()
+                    return 0
+        except:
+            pass
+        finally:
+            log_file=open(working_log,"a")
+            log_file.write(new_file_name+"\n")
+            log_file.close()
+            
+    command= create_command(arquivo,extension_convert=extension_convert,resize=resize,resolution=resolution,codec=codec,fps=fps,crf=crf,preset=preset,array=True)
+    #print(command)
+    if command == "":
+        return 0
+    
+    new_file_name=new_file_name+"."+extension_convert
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    if resize:
         log_file=open(working_log,"a")
         tmp_log_file=open(str(working_log)+".tmp","a")
-        for line in log_file:
+        for line in log_file.readlines():
             if line == new_file_name+"\n":
                 pass
             else:
                 tmp_log_file.write(line)
         log_file.close()
         tmp_log_file.close()
-        os.remove(working_log)
-        os.rename(str(working_log)+".tmp",working_log)
-		log_file=open(resize_log,"a")
-		log_file.write(new_file_name+"\n")
-		log_file.close()
-	if remove:
-		os.remove(str(arquivo.path))
+        shutil.move(str(working_log)+".tmp",working_log)
+        log_file=open(resize_log,"a")
+        log_file.write(new_file_name+"\n")
+        log_file.close()
+    if remove:
+        if same_extension:
+            shutil.move(new_file_name,str(relative_dir)+str(file_name_no_extension)+"."+extension_convert)
+        else:
+            os.remove(str(arquivo.path))
 
 
 pasta_teste="./test"
@@ -120,4 +159,5 @@ print(dir_data)
 #print([f for f in os.listdir(pasta_teste) if os.path.isfile(f)])
 
 for arquivo in dir_data["files"]:
-	convert_video(arquivo,resize=True)
+    print(arquivo)
+    convert_video(arquivo,"mkv",resize=True)
