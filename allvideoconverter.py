400f2cd94c482d77628b748c7e7589859e6a81ec
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2021-01-05 12:00:23 -0300

    begining process of treating log output,created method to do an better identify for media using imdb

--- allvideoconverter.py
+++ allvideoconverter.py
@@ -5,7 +5,7 @@ import re
 import shutil
 import subprocess
 import fnmatch
-
+import contextlib
 
 
 class converter:
@@ -47,6 +47,25 @@ class converter:
         if str(self.output_folder[-1]) != self.so_folder_separator:
                 self.output_folder=self.output_folder+self.so_folder_separator
 
+    def unbuffered(self,proc,stream='stdout'):
+        newlines = ['\n', '\r\n', '\r']
+        stream = getattr(proc, stream)
+        with contextlib.closing(stream):
+            while True:
+                out = []
+                last = stream.read(1)
+                # Don't loop forever
+                if last == '' and proc.poll() is not None:
+                    break
+                while last not in newlines:
+                    if last == '' and proc.poll() is not None:# Don't loop forever
+                        break
+                    out.append(last)
+                    last = stream.read(1)
+                out = ''.join(out)
+                yield out
+
+
     def fill_files_list(self, folders=""):
         fileExtensions = ["webm", "flv", "vob", "ogg", "ogv", "drc", "gifv", "mng", "avi", "mov", "qt", "wmv", "yuv",
                           "rm", "rmvb", "asf", "amv", "mp4", "m4v", "mp\*", "m\?v", "svi", "3gp", "flv", "f4v", "mkv","divx"]
@@ -87,6 +106,8 @@ class converter:
             " ")
         check_dim.append(arquivo.path)
         dim = subprocess.run(check_dim, stdout=subprocess.PIPE)
+        if dim.returncode !=0:
+            return int(dim.returncode)
         dim = re.compile("\'(.*)\\\\").findall(str(dim.stdout))[0]
         #print(dim)
         return str(dim)
@@ -96,6 +117,8 @@ class converter:
             " ")
         check_dim.append(arquivo.path)
         dim = subprocess.run(check_dim, stdout=subprocess.PIPE)
+        if dim.returncode !=0:
+            return int(dim.returncode)
         dim = re.compile("\'(.*)\\\\").findall(str(dim.stdout))[0]
         # print(dim)
         return str(dim)
@@ -104,6 +127,8 @@ class converter:
         check_dim = str("ffprobe -v error -select_streams s:"+str(stream)+" -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1").split(" ")
         check_dim.append(arquivo.path)
         dim = subprocess.run(check_dim, stdout=subprocess.PIPE)
+        if dim.returncode !=0:
+            return int(dim.returncode)
         dim = re.compile("\'(.*)\\\\").findall(str(dim.stdout))[0]
         # print(dim)
         return str(dim)
@@ -112,6 +137,8 @@ class converter:
         check_dim = "ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0".split(" ")
         check_dim.append(arquivo.path)
         dim = subprocess.run(check_dim, stdout=subprocess.PIPE)
+        if dim.returncode !=0:
+            return int(dim.returncode)
         dim = re.compile("(\d+\,\d+)").findall(str(dim.stdout))[0].split(",")
         # print(dim)
         if int(dim[0]) > width or int(dim[1]) > height:
@@ -123,6 +150,8 @@ class converter:
         check_fps = "ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate".split(" ")
         check_fps.append(arquivo.path)
         dim = subprocess.run(check_fps, stdout=subprocess.PIPE)
+        if dim.returncode !=0:
+            return int(dim.returncode)
         dim = re.compile("(\d+\/\d+)").findall(str(dim.stdout))[0].split("/")
         dim = int(dim[0]) / int(dim[1])
         if dim > fps:
@@ -276,7 +305,10 @@ class converter:
                              self.codec, "-map_metadata", "0", "-pix_fmt", "yuv420p",
                              "-threads", str(self.threads),"-copy_unknown"]
         try:
-            if self.compare_subtitle_codec(arquivo,0) in subtitle_srt:
+            subtitle_codec=self.compare_subtitle_codec(arquivo,0)
+            if isinstance(subtitle_codec,int):
+                return ""
+            elif subtitle_codec in subtitle_srt:
                 command.append("-c:s")
                 command.append("srt")
         except:
@@ -306,7 +338,8 @@ class converter:
                     resize_command.append("300k")
                     resize_command.append("-maxrate")
                     resize_command.append("700k")
-                    if self.compare_resolution(arquivo, 426, 240):
+                    compared_res=self.compare_resolution(arquivo, 426, 240)
+                    if not isinstance(compared_res,int) and compared_res:
                         resize_command.append("-s")
                         resize_command.append("426x240")  # determina a escala do arquivo para 480p
                     else:
@@ -318,7 +351,8 @@ class converter:
                     resize_command.append("400k")
                     resize_command.append("-maxrate")
                     resize_command.append("1000k")
-                    if self.compare_resolution(arquivo, 640, 320):
+                    compared_res=self.compare_resolution(arquivo, 640, 320)
+                    if not isinstance(compared_res,int) and compared_res:
                         resize_command.append("-s")
                         resize_command.append("640x320")  # determina a escala do arquivo para 480p
                     else:
@@ -330,7 +364,8 @@ class converter:
                     resize_command.append("500k")
                     resize_command.append("-maxrate")
                     resize_command.append("2000k")
-                    if self.compare_resolution(arquivo, 854, 480):
+                    compared_res= self.compare_resolution(arquivo, 854, 480)
+                    if not isinstance(compared_res,int) and compared_res:
                         resize_command.append("-s")
                         resize_command.append("854x480")  # determina a escala do arquivo para 480p
                     else:
@@ -342,7 +377,8 @@ class converter:
                     resize_command.append("1500k")
                     resize_command.append("-maxrate")
                     resize_command.append("6000k")
-                    if self.compare_resolution(arquivo, 1280, 720):
+                    compared_res= self.compare_resolution(arquivo, 1280, 720)
+                    if not isinstance(compared_res,int) and compared_res:
                         resize_command.append("-s")
                         resize_command.append("1280x720")  # determina a escala do arquivo para 720p
                     else:
@@ -354,7 +390,8 @@ class converter:
                     resize_command.append("3000k")
                     resize_command.append("-maxrate")
                     resize_command.append("9000k")
-                    if self.compare_resolution(arquivo, 1920, 1080):
+                    compared_res= self.compare_resolution(arquivo, 1920, 1080)
+                    if not isinstance(compared_res,int) and compared_res:
                         resize_command.append("-s")
                         resize_command.append("1920x1080")  # determina a escala do arquivo para 1080p
                     else:
@@ -375,7 +412,10 @@ class converter:
         if force_change_fps and not change_fps:
             command.append("-filter:v")
             command.append("fps=fps=" + str(self.fps))
-        if self.compare_video_codec(arquivo) == "mpeg4" and "h26" in self.codec :
+        compared_codec=self.compare_video_codec(arquivo)
+        if isinstance(compared_codec,int):
+            return ""
+        elif compared_codec == "mpeg4" and "h26" in self.codec :
             command.append("-bsf:v")
             command.append("mpeg4_unpack_bframes")
         #command.append(str(name_data["new_file_name"])+".tmp")
@@ -439,7 +479,7 @@ class converter:
                                     name_data["file_name_no_extension"]) + "." + self.extension_convert)
                 else:
                     os.remove(str(arquivo.path))
-            if result.returncode == 1 and name_data["new_file_name"] != "":
+            elif result.returncode == 1 and name_data["new_file_name"] != "":
                 try:
                     os.remove(str(name_data["new_file_name"]+".tmp"))
                 except:
@@ -535,9 +575,7 @@ class converter_identifier(converter):
     from imdbpie import Imdb
     from json import JSONDecoder
     import urllib
-    def __init__(self,imdb_api_key, extension_convert: str = "mkv", resolution: int = 480, codec: str = "h264_omx", fps: int = 24,
-                 crf: int = 20, preset: str = "slow", hwaccel="", threads=2, log_level=32, input_folder="./",
-                 output_folder="./convert/", resize_log="./resize.log", resized_log="./resized.log"):
+    def __init__(self,imdb_api_key, extension_convert: str = "mkv", resolution: int = 480, codec: str = "h264_omx", fps: int = 24,crf: int = 20, preset: str = "slow", hwaccel="", threads=2, log_level=32, input_folder="./",output_folder="./convert/", resize_log="./resize.log", resized_log="./resized.log"):
         super().__init__(extension_convert, resolution, codec, fps,
                  crf, preset, hwaccel, threads, log_level, input_folder,
                  output_folder, resize_log, resized_log)
@@ -581,10 +619,13 @@ class converter_identifier(converter):
 
         imdb = self.Imdb()
         movie_results = []
-        results = imdb.search_for_title(title)
-        for result in results:
-            if result['type'] == "feature":
-                movie_results.append(result)
+        while not movie_results and len(title)>1:
+            results = imdb.search_for_title(title)
+            for result in results:
+                if result['type'] == "feature":
+                    movie_results.append(result)
+            if not movie_results:
+                title=title[:-1]
 
         if not movie_results:
             while not movie_results:
@@ -605,8 +646,11 @@ class converter_identifier(converter):
 
         imdb_movie_title = imdb_movie['base']['title']
         imdb_movie_id = mpr['imdb_id']
-        imdb_movie_year = imdb_movie['base']['year']
-        imdb_movie_rating = imdb_movie['ratings']['rating']
+        imdb_movie_year = mpr['year']
+        try:
+            imdb_movie_rating = imdb_movie['ratings']['rating']
+        except:
+            imdb_movie_rating=0
         if not 'outline' in imdb_movie['plot']:
             imdb_movie_plot_outline = (imdb_movie['plot']['summaries'][0]
             ['text'])
@@ -654,26 +698,26 @@ class converter_identifier(converter):
             command.append("-map 0:{}".format(f))
 
 
-        #if self.extension_convert != "mkv":
-        poster_filename = title + ".jpg"
-        if not os.path.isfile(poster_filename):
-            print('\nFetching the movie poster...')
-            tmdb_find = self.tmdb.Find(imdb_movie_id)
-            tmdb_find.info(external_source='imdb_id')
+        try:
+            poster_filename = title + ".jpg"
+            if not os.path.isfile(poster_filename):
+                print('\nFetching the movie poster...')
+                tmdb_find = self.tmdb.Find(imdb_movie_id)
+                tmdb_find.info(external_source='imdb_id')
 
-            path = tmdb_find.movie_results[0]['poster_path']
-            complete_path = r'https://image.tmdb.org/t/p/w780' + path
+                path = tmdb_find.movie_results[0]['poster_path']
+                complete_path = r'https://image.tmdb.org/t/p/w780' + path
 
-            uo = self.urllib.request.urlopen(complete_path)
-            with open(poster_filename, "wb") as poster_file:
-                poster_file.write(uo.read())
-                poster_file.close()
-        command.append("-attach")
-        command.append(poster_filename)
-        command.append("-metadata:s:t:0")
-        command.append("mimetype=image/jpg")
-            #command.append("-metadata:s:t:0")
-            #command.append("filename=")
+                uo = self.urllib.request.urlopen(complete_path)
+                with open(poster_filename, "wb") as poster_file:
+                    poster_file.write(uo.read())
+                    poster_file.close()
+            command.append("-attach")
+            command.append(poster_filename)
+            command.append("-metadata:s:t:0")
+            command.append("mimetype=image/jpg")
+        except:
+            pass
 
 
 
@@ -703,8 +747,77 @@ class converter_identifier(converter):
         if not process:
             #print(command)
             #os.chdir(self.output_folder)
-            result = subprocess.run(command, stdout=subprocess.PIPE)
-            os.remove(os.path.splitext(arquivo.name)[0] + ".jpg")
+            result = subprocess.Popen(command, stdout = subprocess.PIPE,stderr = subprocess.STDOUT,universal_newlines=True)
+            #line_nun=0
+            number_of_frames=0
+            error=0
+            for line in iter(result.stdout.readline, b''):
+                if "frame" in line and "fps" in line and "q" in line and "size"in line and "time"in line and "bitrate"in line and "speed"in line :
+                    '''frame= 2246 fps=748 q=15.0 size=   23365kB time=00:01:33.97 bitrate=2036.9kbits/s speed=31.3x'''
+                    #print(number_of_frames)
+                    try:
+                        search=re.search(r'frame=\D*(\d*)\D*fps=\D*(\d*)\D*q=\D*([\d\.]*)\D*size=\D*(\d*)\D*time=\D*(.*?)bitrate=\D*([\d\.kmg]*)*bits/s\D*speed=\D*([\d\.]*)\D*',line)
+                        try:
+                            frame=int(search.group(1))
+                        except:
+                            frame=int(re.search(r'frame=\D*(\d+)\D*=',line).group(1))
+                        try:
+                            fps=int(search.group(2))
+                        except:
+                            fps=int(re.search(r'fps=\D*(\d+)\D*=',line).group(1))
+                        try:
+                            quality=float(search.group(3))
+                        except:
+                            quality=float(re.search(r'q=\D*([\d\.]+)\D*=',line).group(1))
+                        try:
+                            size=int(search.group(4))
+                        except:
+                            size=int(re.search(r'size=\D*(\d+)\D*=',line).group(1))
+                        try:
+                            time=search.group(5)
+                        except:
+                            time=re.search(r'time=\D*(.*?)bitrate',line).group(1)
+                        try:
+                            bitrate=search.group(6)
+                        except:
+                            bitrate=re.search(r'bitrate=\D*([\d\.kmg]+)*bits/s\D*=',line).group(1)
+                        try:
+                            speed=float(search.group(7))
+                        except:
+                            speed=float(re.search(r'speed=\D*([\d\.]*)\D*',line).group(1))
+                        conclusion=int((number_of_frames*int(frame))/100)
+                        try:
+                            eta=int((number_of_frames-int(frame))/int(fps))
+                        except:
+                            eta="nan"
+                        out=str(str(line_nun)+str(conclusion)+"    eta="+str(eta)+"s  quality="+str(quality)+"  speed="+str(speed))
+                        print("test",end="\r")
+                    except:
+                        print(str(search),end="\r")
+                elif "NUMBER_OF_FRAMES"in line:
+                    #print(str(line_nun)+str(line.rstrip()))
+                    frames=re.search(r'NUMBER_OF_FRAMES\D*(\d+).*',line)
+                    try:
+                        number_of_frames=int(frames.group(1))
+                    except:
+                        pass
+                elif "Error" in line:
+                    error+=1
+                    print(str(error)+line)
+                elif "Conversion failed" in line:
+                    result.kill()
+                    return 1
+                elif len(line)>0 and line[-1]!="\r" :
+                    if line[-1]=="\n":
+                        print(str(line[:-1]))
+                    else:
+                        print(str(line))
+                    pass
+                #line_nun+=1
+            try:
+                os.remove(os.path.splitext(arquivo.name)[0] + ".jpg")
+            except:
+                pass
             if resize and result.returncode == 0:
                 # log_file=open(working_log,"a")
                 # tmp_log_file=open(str(working_log)+".tmp","a")
@@ -757,6 +870,8 @@ class converter_identifier(converter):
                     self.files.remove(file)
         else:
             print(self.results)
+
+tmdb_API_KEY = 'b888b64c9155c26ade5659ea4dd60e64'
 
 tmdb_API_KEY = 'b888b64c9155c26ade5659ea4dd60e64'
 
